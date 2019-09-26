a59ffbd9d45cd34966a9babcb643efd627dd05a8
DemiQin <yqin2@tulane.edu>
2019-09-26 14:31:53 -0500

    test

---
+++ Applications/tda_python_cli/tda_python_cli.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+from ripser import ripser
+from persim import plot_diagrams
+
+from histomicstk.segmentation import label as htk_label
+
+import logging
+logging.basicConfig(level=logging.CRITICAL)
+
+from ctk_cli import CLIArgumentParser
+
+def compute_PD_features(im_label):
+
+    feature_list = []
+
+    data_test = np.random.random((100,2))
+    data = im_label
+    diagrams = ripser(data)['dgms']
+    plot_diagrams(diagrams, show=True)
+
+    fdata = pd.concat(feature_list, axis=1)
+
+    print fdata
+
+if __name__ == "__main__":
+    main(CLIArgumentParser().parse_args())
+
